9f0f71a28566d94d13b2a5439fee44bfefa9f174
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-29 19:15:11 +0800

    Removed Batch 6 Files

--- practice_python_program_batch_06/prog03_convert_to_lowercase_without_lower.py
+++
@@ -1,7 +0,0 @@
-#Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
-
-#Ask User for text
-
-#Convert uppercase to lowercase
-
-#Print text
